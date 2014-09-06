c2f3dab0d6fa8466c42678aaa827b5493588d9e9
Vicente69 <vboriesazeau@gmail.com>
2014-09-06 21:36:35 +0200

    Added repository class for common extensions.

---
+++ src/IO/FileExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Inneair\Synapps\IO;
+
+/**
+ * This class contains common file extensions.
+ */
+final class FileExtension
+{
+    const HTM = 'htm';
+    const HTML = 'html';
+    const ZIP = 'zip';
+
+    /**
+     * Private constructor prevents erroneous instanciations.
+     */
+    private function __construct()
+    {
+    }
+}
